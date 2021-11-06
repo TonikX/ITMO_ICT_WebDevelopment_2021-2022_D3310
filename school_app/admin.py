5141fb2b50ed6fc8f509c340510a321ecd076ec5
ZJiaYi <91015286+ZJiaYi@users.noreply.github.com>
2021-11-06 21:56:18 +0300

    Delete school_app directory

--- school_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Teacher)
-admin.site.register(Group)
-admin.site.register(Classroom)
-admin.site.register(Schedule)
-admin.site.register(Student)
