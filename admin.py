32ec8f56919b49f9960f55f5cee24ad57511d9ab
Ван Цюаньюй <64023974+gdnjr5233@users.noreply.github.com>
2021-11-29 07:07:00 +0800

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import car_owner, ownership, car, drivers_license, User
-admin.site.register(car_owner)
-admin.site.register(ownership)
-admin.site.register(car)
-admin.site.register(drivers_license)
-admin.site.register(User)
