192eb8be892e36177ecc6897c8b42a3e2379f99d
ZJiaYi <91015286+ZJiaYi@users.noreply.github.com>
2021-12-14 22:42:04 +0300

    Update Student.vue

--- students/группа/лаб/Чжан Цзяи/Лаб4/school_vue/src/components/Student.vue
+++ students/группа/лаб/Чжан Цзяи/Лаб4/school_vue/src/components/Student.vue
@@ -1,207 +1,321 @@
 <template>
-  <div>
-    <v-form @submit="submit()">
-      <v-text-field
-      v-model="first_name"
-      :counter="10"
-      label="Имя"
-      required
-      ></v-text-field>
-      <v-text-field
-      v-model="last_name"
-      :counter="10"
-      label="Фамиля"
-      required
-      ></v-text-field>
-      <v-text-field
-      v-model="score_math"
-      :counter="10"
-      label="Баллы матиматики"
-      ></v-text-field>
-      <v-text-field
-      v-model="score_chemical"
-      :counter="10"
-      label="Баллы химии"
-      ></v-text-field>
-      <v-text-field
-      v-model="score_english"
-      :counter="10"
-      label="Баллы англиского"
-      ></v-text-field>
-      <v-text-field
-      v-model="score_computer"
-      :counter="10"
-      label="Баллы компьютера"
-      ></v-text-field>
-      <v-text-field
-      v-model="semester"
-      :counter="2"
-      label="Семестер"
-      ></v-text-field>
-      <v-select
-      v-model="sex"
-      :items="sexes"
-      label="Пол"
-      required
-      ></v-select>
-      <v-select
-      v-model="group"
-      :items="groups"
-      label="Группа"
-      required
-      ></v-select>
-      <v-btn
-        color="success"
-        class="mr-4"
-        type="submit"
+  <v-data-table
+    :headers="headers"
+    :items="teachers"
+    item-key="id"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
       >
-        Сохранить
-      </v-btn>
-      <v-btn
-        color="error"
-      class="mr-4"
-      @click="reset"
-      >
-      Сбросить
-      </v-btn>
-    </v-form>
-    <div v-for="student in students" :key="student.id">
-      <div class="shades black text-center">
-        <span class="white--text">first name: {{ student.first_name }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">last name: {{ student.last_name }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">score math: {{ student.score_math }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">score chemical: {{ student.score_chemical }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">score english: {{ student.score_english }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">score computer: {{ student.score_computer }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">semester: {{ student.semester }}</span>
-      </div>
-      <div class="shades black text-center">
-        <span class="white--text">group: {{ student.group }}</span>
-      </div>
-      <v-btn
-        color="error"
-      class="mr-4"
-      @click="deleteStudent(student.id)"
-      >
-      Удалить
-      </v-btn>
-      <v-form @submit="update(student.id)">
-        <v-text-field
-        v-model="first_name"
-        :counter="10"
-        label="Имя"
-        required
-        ></v-text-field>
-        <v-text-field
-        v-model="last_name"
-        :counter="10"
-        label="Фамиля"
-        required
-        ></v-text-field>
-        <v-text-field
-        v-model="score_math"
-        :counter="10"
-        label="Баллы матиматики"
-        ></v-text-field>
-        <v-text-field
-        v-model="score_chemical"
-        :counter="10"
-        label="Баллы химии"
-        ></v-text-field>
-        <v-text-field
-        v-model="score_english"
-        :counter="10"
-        label="Баллы англиского"
-        ></v-text-field>
-        <v-text-field
-        v-model="score_computer"
-        :counter="10"
-        label="Баллы компьютера"
-        ></v-text-field>
-        <v-text-field
-        v-model="semester"
-        :counter="2"
-        label="Семестер"
-        ></v-text-field>
-        <v-select
-        v-model="sex"
-        :items="sexes"
-        label="Пол"
-        required
-        ></v-select>
-        <v-select
-        v-model="group"
-        :items="groups"
-        label="Группа"
-        required
-        ></v-select>
-        <v-btn
-          color="success"
-          class="mr-4"
-          type="submit"
+        <v-toolbar-title>Список Приподаватель</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer>
+          <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              @click="addNewTeacher"
+          >
+              Новый приподаватель
+          </v-btn>
+        </v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
         >
-          Сохранить
-        </v-btn>
-        <v-btn
-          color="error"
-        class="mr-4"
-        @click="reset"
-        >
-        Сбросить
-        </v-btn>
-      </v-form>
-    </div>
-  </div>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5"></span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="first_name"
+                      :counter="10"
+                      label="Имя"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="last_name"
+                      :counter="10"
+                      label="Фамиля"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="b_date"
+                      :counter="10"
+                      label="Время начала"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="e_date"
+                      :counter="10"
+                      label="Время закончила"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="office"
+                      :counter="10"
+                      label="кабинет"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-select
+                      v-model="subject"
+                      :items="subjects"
+                      label="Дисциплина"
+                      required
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+              color="blue darken-1"
+              text
+              @click="cancel"
+              >
+              Не
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="submit"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-btn
+        small
+        @click="addTeacher"
+      >
+        Обновить
+      </v-btn>
+      <v-dialog
+      max-width="500px"
+      v-model="show"
+      >
+        <v-card>
+          <v-card-title>
+            <span class="text-h5"></span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="first_name"
+                    :counter="10"
+                    label="Имя"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="last_name"
+                    :counter="10"
+                    label="Фамиля"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="b_date"
+                    :counter="10"
+                    label="Время начала"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="e_date"
+                    :counter="10"
+                    label="Время закончила"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    v-model="office"
+                    :counter="10"
+                    label="кабинет"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-select
+                    v-model="subject"
+                    :items="subjects"
+                    label="Дисциплина"
+                    required
+                  ></v-select>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="cancel"
+            >
+              Не
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="update(item.id)"
+            >
+              Сохранить
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-btn
+        small
+        @click="deleteTeacher(item.id)"
+      >
+        удалить
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
-  name: 'Student',
+  name: 'Teacher',
   data () {
     return {
-      students: {},
+      show: false,
+      dialog: false,
+      headers: [
+        {
+          text: 'Приподаватель',
+          align: 'start',
+          sortable: false,
+          value: 'id'
+        },
+        { text: 'Имя', value: 'first_name' },
+        { text: 'Фамилия', value: 'last_name' },
+        { text: 'Дисциплина', value: 'subject' },
+        { text: 'Время начала', value: 'b_date' },
+        { text: 'Время закончила', value: 'e_date' },
+        { text: 'Кабинет', value: 'office' },
+        { text: 'Actions', value: 'actions', sortable: false }
+      ],
+      teachers: [],
       first_name: '',
       last_name: '',
-      group: null,
-      groups: [{ text: 'Выберите группу', value: null, disabled: true }],
-      sex: null,
-      semester: null,
-      score_math: null,
-      score_english: null,
-      score_computer: null,
-      score_chemical: null,
-      sexes: [
-        { text: 'Мужчина', value: 'm' },
-        { text: 'Женщина', value: 's' }
+      b_date: '',
+      e_date: '',
+      office: null,
+      subject: null,
+      subjects: [
+        { text: 'математика', value: 'm' },
+        { text: 'химия', value: 'c' },
+        { text: 'английский ', value: 'e' },
+        { text: 'компьютерная наука', value: 'p' }
       ]
     }
   },
   methods: {
+    addNewTeacher () {
+      this.dialog = true
+    },
+    cancel () {
+      this.show = false
+      this.dialog = false
+    },
+    addTeacher () {
+      this.show = true
+    },
     submit () {
+      window.event.preventDefault()
       const data = {
         first_name: this.first_name,
         last_name: this.last_name,
-        group: this.group,
-        sex: this.sex,
-        semester: this.semester,
-        score_math: this.score_math,
-        score_english: this.score_english,
-        score_computer: this.score_computer
+        b_date: this.b_date,
+        e_date: this.e_date,
+        subject: this.subject,
+        office: this.office
       }
       this.axios
-        .post('http://127.0.0.1:8000/api/students/new', data)
+        .post('http://127.0.0.1:8000/api/teachers/new', data)
         .then(response => { console.log(response); this.reset() })
         .catch(err => { console.log(err) })
     },
@@ -209,73 +323,51 @@ export default {
       const data = {
         first_name: this.first_name,
         last_name: this.last_name,
-        group: this.group,
-        sex: this.sex,
-        semester: this.semester,
-        score_math: this.score_math,
-        score_english: this.score_english,
-        score_computer: this.score_computer
+        b_date: this.b_date,
+        e_date: this.e_date,
+        subject: this.subject,
+        office: this.office
       }
       this.axios
-        .patch('http://127.0.0.1:8000/api/students/update/' + id, data)
+        .patch('http://127.0.0.1:8000/api/teachers/update/' + id, data)
         .then(response => { console.log(response); this.reset() })
         .catch(err => { console.log(err) })
     },
-    deleteStudent (id) {
-      const url = 'http://127.0.0.1:8000/api/students/delete/' + id
+    deleteTeacher (id) {
+      const url = 'http://127.0.0.1:8000/api/teachers/delete/' + id
       this.axios
         .delete(url)
         .then(response => { console.log(response) })
     },
     reset () {
-      this.sex = null
-      this.group = null
-      this.semester = null
-      this.score_math = null
-      this.score_english = null
-      this.score_computer = null
+      this.subject = null
       this.last_name = ''
       this.first_name = ''
+      this.b_date = ''
+      this.e_date = ''
+      this.office = null
     },
-    showGroups (groups) {
-      for (let i = 0; i < groups.length; i++) {
-        const group = {}
-        group.value = groups[i].id
-        group.text = groups[i].number
-
-        this.groups.push(group)
-      }
-    },
-    showStudents (data) {
-      this.students = []
+    showTeachers (data) {
+      this.teachers = []
       for (let i = 0; i < data.length; i++) {
-        const student = {}
-        student.id = data[i].id
-        student.first_name = data[i].first_name
-        student.last_name = data[i].last_name
-        student.sex = data[i].sex
-        student.score_math = data[i].score_math
-        student.score_chemical = data[i].score_chemical
-        student.score_english = data[i].score_english
-        student.score_computer = data[i].score_computer
-        student.semester = data[i].semester
-        student.group = data[i].group
-        console.log(student)
-        this.students.push(student)
+        const teacher = {}
+        teacher.id = data[i].id
+        teacher.first_name = data[i].first_name
+        teacher.last_name = data[i].last_name
+        teacher.b_date = data[i].b_date
+        teacher.e_date = data[i].e_date
+        teacher.office = data[i].office
+        teacher.subject = data[i].subject
+        console.log(teacher)
+        this.teachers.push(teacher)
       }
     }
   },
   mounted () {
     this.axios
-      .get('http://127.0.0.1:8000/api/groups/list')
+      .get('http://127.0.0.1:8000/api/teachers/list')
       .then(response => {
-        console.log(response); this.showGroups(response.data)
-      })
-      .catch(err => { console.log(err) })
-    this.axios
-      .get('http://127.0.0.1:8000/api/students/list')
-      .then(response => {
-        console.log(response); this.showStudents(response.data)
+        console.log(response); this.showTeachers(response.data)
       })
       .catch(err => { console.error(err) })
   }
